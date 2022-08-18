5f259ae05a4cbec85f48248692d13cc4cefdf1d2
Viktor <viktopiniaginya@gmail.com>
2022-08-18 12:54:15 +0300

    ready basic project
    ready basic project

--- URLSessionTask.playground/Contents.swift
+++ URLSessionTask.playground/Contents.swift
@@ -5,10 +5,20 @@ func getData(urlRequest: String) {
     guard let url = urlRequest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
-            
+            print("Это ошибка\n\(error ?? fatalError())")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
+            print("""
+                  Это код ответа
+                  ---------------------------
+                  \(response)
+                  Это пришедшие данные
+                  ---------------------------
+                  \(dataAsString ?? "данные отсутствуют")
+                  """)
         }
     }.resume()
 }
+
+getData(urlRequest: "https://cat-fact.herokuapp.com")
